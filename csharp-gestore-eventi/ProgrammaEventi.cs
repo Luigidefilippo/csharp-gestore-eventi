d2b6bb54d4143db1d1abee6ee9bf8188d6c03fe5
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-18 12:38:25 +0200

    Continue mileston 3

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_gestore_eventi
+{
+    public class ProgrammaEventi
+    {
+        public string Titolo { get;  private set; }
+        private List<Evento> eventi;
+
+        public ProgrammaEventi (string titolo )
+        {
+            Titolo = titolo;
+            eventi = new List<Evento> ();
+        }
+        public void AggiungiEvento(Evento evento )
+        {
+            eventi.Add (evento);
+        }
+        public List<Evento> EventiInData( DateTime data)
+        {
+            return eventi.FindAll( e => e.Data == data.Date );
+        }
+
+        public static string StampaEventi(List<Evento> eventi)
+        {
+            if( eventi.Count == 0 )
+            {
+                return "Nessun evento trovato";
+            }
+
+            
+        }
+    }
+}
