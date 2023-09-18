6c0a54d390416dae155e45e85943c9e0c66d4296
Luigidefilippo <defilippoluigi65@gmail.com>
2023-09-18 10:56:34 +0200

    Creation new event program.cs

--- csharp-gestore-eventi/Program.cs
+++ csharp-gestore-eventi/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Benvenuto nel programma di gestione eventi");
+            //Inseriamo un nuovo evento 
+
+            Console.Write("Inserisci il titolo dell'evento");
+            string titolo = Console.ReadLine();
+
+            //Inseriamo la data dell'evento 
+
+            Console.Write("Inserisci la data dell'evento (formato dd/MM/yyyy: ");
+            DateTime data = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
         }
     }
 }
